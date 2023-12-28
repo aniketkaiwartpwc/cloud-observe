8fca491cedebe59ce97d3ac29b4f9f587fd40b13
aniketkaiwartpwc <152481305+aniketkaiwartpwc@users.noreply.github.com>
2023-12-28 17:16:49 +0530

    Update exercise_gcs_txt_to_bigquery_dag.py

--- dags/exercise_gcs_txt_to_bigquery_dag.py
+++ dags/exercise_gcs_txt_to_bigquery_dag.py
@@ -5,26 +5,18 @@ from airflow.utils.dates import days_ago
 #from airflow.contrib.operators.dataflow_operator import DataFlowPythonOperator -- This class is deprecated, use DataflowCreatePythonJobOperator instead
 from airflow.providers.google.cloud.operators.dataflow import DataflowCreatePythonJobOperator
 from airflow import configuration
-from airflow.models import Variable
 
 default_args = {
     'owner': 'aniket',
     'email': ['aniket.kaiwart@pwc.com'],
-
 }
-
-
-CONF_BASE_DIR = os.path.dirname(configuration.conf.get('core', 'dags_folder'))
-BASE_DIR = f"gs://{os.environ.get('BUCKET_NAME')}" if os.environ.get('ENVIRONMENT') == 'Production' else CONF_BASE_DIR
-DATAFLOW_FUNCTIONS_PATH =  f'{BASE_DIR}/dataflow-functions/process_citizen_txt.py' if "gs://" in BASE_DIR else os.path.join(CONF_BASE_DIR, 'dataflow-functions', 'process_citizen_txt.py')
 
 
 @dag(default_args=default_args, schedule_interval='0 6 * * *', start_date=days_ago(1), tags=['dataflow-job'])
 def exercise_gcs_txt_to_bigquery_dag():
     txt_bq_dataflow_job = DataflowCreatePythonJobOperator(
         task_id='etl_txt_to_bq_dataflow_job',
-        gcp_conn_id='google_cloud_default',
-        py_file=DATAFLOW_FUNCTIONS_PATH,
+        py_file='gs://asia-south1-cloud-dataobs-1bb414f6-bucket/dataflow-functions/process_citizen_txt.py',
         job_name='{{task.task_id}}',
         py_interpreter='python3',
         location='asia-south1'
