156593997e663528d2c66ac544a8903bef4b2a90
aniketkaiwartpwc <152481305+aniketkaiwartpwc@users.noreply.github.com>
2024-03-14 13:23:26 +0530

    Update log_table_dag_level.py

--- dataflow-functions/log_table_dag_level.py
+++ dataflow-functions/log_table_dag_level.py
@@ -57,7 +57,7 @@ def run():
                                                                                 'airflowlog_test_daglevel',
                                                                                 dataset = 'airflow_log_capture',
                                                                                 project= 'pg-us-n-app-119329',
-                                                                                schema= 'SCHEMA_AUTODETECT',
+                                                                                schema= 'Run_Timestamp:TIMESTAMP, DagName:STRING, DagStatus:STRING, Start_Date:TIMESTAMP, End_Date:TIMESTAMP',
                                                                                 create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED,
                                                                                 write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND
                                                                             )
