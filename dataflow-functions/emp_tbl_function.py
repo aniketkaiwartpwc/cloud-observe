f55e7a6aef48e83b1f31d380d27e6fb24196e668
aniketkaiwartpwc <152481305+aniketkaiwartpwc@users.noreply.github.com>
2024-03-14 14:26:11 +0530

    Update emp_tbl_function.py

--- dataflow-functions/emp_tbl_function.py
+++ dataflow-functions/emp_tbl_function.py
@@ -1,11 +1,9 @@
 import apache_beam as beam
 from apache_beam.pipeline import PipelineOptions
 from apache_beam.options.pipeline_options import SetupOptions
-from statistics import  mean
 import logging
 import argparse
 import re
-
 
 table_spec = 'dataobs.employee_table'
 table_schema='Employee_ID:INT64, First_Name:STRING, Last_Name:STRING, Email:STRING, Age:INT64, City:STRING, State:STRING, Country:STRING, Occupation:STRING, Job_Run_Date:TIMESTAMP'
@@ -15,16 +13,6 @@ class DataIngestion:
         values = re.split(",", re.sub('\r\n', '', re.sub('"', '',string_input)))
         row = dict(zip(('Employee_ID', 'First_Name', 'Last_Name', 'Email', 'Age', 'City', 'State', 'Country', 'Occupation', 'Job_Run_Date'),values))
         return row
-
-class GreaterThanAvg(beam.DoFn):
-    def process(self, element,side_input):
-        avg=mean(side_input)
-
-        if int(element[0]) > avg:
-            yield element
-
-def replace_space_with_zero(line):
-  return line.replace(' ', '0')
 
 def run(argv=None, save_main_session=True):
     parser = argparse.ArgumentParser()
@@ -42,23 +30,12 @@ def run(argv=None, save_main_session=True):
         maindata=(
             p| "Reading file" >> beam.io.ReadFromText(known_args.input,skip_header_lines=1)
             |"Split" >> beam.Map(lambda x:x.split(","))
-            )
-
-        side_input=(
-         maindata | "Side input pricing " >> beam.Map(lambda x:int(x[0]))
-        )
-
-        greater_than_avg=(
-            maindata| "greater than avg" >> beam.ParDo(GreaterThanAvg(),beam.pvalue.AsList(side_input)) \
-            | "join the data">> beam.Map(lambda x:','.join(x))  \
             | "Data Ingestion " >> beam.Map(lambda s: dataingestion.parse_method(s)) \
-            #| "write to file " >> beam.io.WriteToText(known_args.output)
             | "Write to BQ" >> beam.io.WriteToBigQuery(table_spec, schema=table_schema,
-                                                      write_disposition=beam.io.BigQueryDisposition.WRITE_APPEND,
+                                                      write_disposition=beam.io.BigQueryDisposition.WRITE_TRUNCATE,
                                                        create_disposition=beam.io.BigQueryDisposition.CREATE_IF_NEEDED)
             )
 
-
 if __name__ == '__main__':
   logging.getLogger().setLevel(logging.INFO)
-  run()
+  run() 
